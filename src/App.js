5b80669d4e7d0828b76afd3a742680419468cd8b
faysalwzbm <ovitahsan@gmail.com>
2022-06-07 23:59:52 +0200

    create the Nav component and import the require files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
 export default App;
