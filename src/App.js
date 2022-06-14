dbb81f37940adbe8a24990963fdea92158df013a
faysalwzbm <ovitahsan@gmail.com>
2022-06-14 16:26:03 +0200

    add the app.css file and code for the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
