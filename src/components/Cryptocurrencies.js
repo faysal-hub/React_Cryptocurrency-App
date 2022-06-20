51f355b0716ecded15bfa826b9e7cb9c0e98e29c
faysalwzbm <ovitahsan@gmail.com>
2022-06-20 22:19:16 +0200

    complete the cryptocurrencies page

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -8,12 +8,30 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 function Cryptocurrencies({ simplified }) {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
   if (isFetching) return 'Loading...';
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col
