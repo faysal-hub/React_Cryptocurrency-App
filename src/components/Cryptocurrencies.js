f87d8294db8f5484a84a3a02b659cba47a9a719f
faysalwzbm <ovitahsan@gmail.com>
2022-06-20 21:23:35 +0200

    show all the card into the DOM

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -9,9 +9,33 @@ function Cryptocurrencies() {
   const { data: cryptosList, isFetching } = useGetCryptosQuery();
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
-  console.log(cryptos);
-
-  return <div>Cryptocurrencies</div>;
+  return (
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {currency.change}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Cryptocurrencies;
