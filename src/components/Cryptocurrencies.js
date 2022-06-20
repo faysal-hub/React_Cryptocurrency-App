f6fff4ab7c636d9bc13fde0ee11bb0a0855e6338
faysalwzbm <ovitahsan@gmail.com>
2022-06-20 20:52:20 +0200

    import data into the Cryptocurrencies component

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 function Cryptocurrencies() {
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+  console.log(cryptos);
+
   return <div>Cryptocurrencies</div>;
 }
 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
+import { Cryptocurrencies, News } from '../components';
+
 const { Title } = Typography;
 
 function Homepage() {
@@ -47,6 +49,24 @@ function Homepage() {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
