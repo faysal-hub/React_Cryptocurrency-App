b7462d0936a4b1af8815c19f56dba778188fee70
faysalwzbm <ovitahsan@gmail.com>
2022-06-20 13:22:04 +0200

    configure the redux-toolkit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0',
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': 'd334132cbdmsh498f4e38f1dac60p1b490djsn090f94628bc6',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//   },
+// };
