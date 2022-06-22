78494f6282da34342c6bfdc1ed4c38767dc38af8
faysalwzbm <ovitahsan@gmail.com>
2022-06-22 19:38:16 +0200

    loaded crypto News cards into DOM

--- src/components/News.js
+++ src/components/News.js
@@ -1,14 +1,36 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import Loader from './Loader';
+
+const { Text, Title } = Typography;
 
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: 'Cryptocurrency',
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
-  console.log(cryptoNews);
-  return <div>News</div>;
+
+  if (!cryptoNews?.value) return <Loader />;
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
