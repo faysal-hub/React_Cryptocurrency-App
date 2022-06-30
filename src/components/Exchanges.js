e0a59ff9ee4b1be3cc38eb00190534d8917a7e7c
faysalwzbm <ovitahsan@gmail.com>
2022-06-30 18:47:21 +0200

    rewrite exchange component

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Row, Typography } from 'antd';
 
-function Exchanges() {
-  return <div>Exchanges</div>;
-}
+import BgImage from '../images/coming-soon.png';
+
+// Exchanges (Commin Soon)
+const Exchanges = () => {
+  return (
+    <Row gutter={[32, 32]} className="exchanges-card-container">
+      <img src={BgImage} alt="Coming Soon" className="exchanges-image" />
+      <Typography.Title level={1} className="exchanges-heading">
+        Coming Soon
+      </Typography.Title>
+    </Row>
+  );
+};
 
 export default Exchanges;
